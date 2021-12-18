117cb50bdfdcc2e93ab5a55e095333cb2b1ab0aa
haru <haruki6180@gmail.com>
2021-12-18 16:14:50 +0900

    トップページOK

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text("top page")),
+      ),
+    );
+  }
+}
