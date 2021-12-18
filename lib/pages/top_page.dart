fc77e21b3adee40a429fe15ab92897579f94ebc7
haru <haruki6180@gmail.com>
2021-12-18 18:06:55 +0900

    大まかなデザインOK

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -12,7 +12,59 @@ class _TopPageState extends State<TopPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: Text("top page")),
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/top-img.webp"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: SafeArea(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 130.0,
+                    ),
+                    Container(child: Text("Qiita App",
+                      style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.white,
+                      ),
+                    )),
+                    Container(child: Text("Yoh Haru",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ))
+                  ],
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    TextButton(onPressed: () { }, child: Text("ログイン", style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green),
+
+                      ),
+                    ),
+                    TextButton(onPressed: () { }, child: Text("ゲストでログイン", style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ),
+
       ),
     );
   }
