05bec4e499a52a19e2d40a51f9ea42a54fc83b52
haru <haruki6180@gmail.com>
2022-01-04 19:15:11 +0900

    タイトル表示

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qiita/model/article.dart';
 import 'package:qiita/services/qiita_client.dart';
 
 class FeedPage extends StatefulWidget {
@@ -9,14 +10,13 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+  late Future<List<Article>> articles;
+
   @override
   void initState(){
     super.initState();
-    QiitaClient.fetchArticle().then((articles) => {
-      articles.forEach((article) => {
-        print(article.title)
-      })
-    });
+    articles = QiitaClient.fetchArticle();
+
   }
 
   @override
@@ -25,7 +25,60 @@ class _FeedPageState extends State<FeedPage> {
     return Scaffold(
       body: SafeArea(  // SafeArea・・・充電とかの上と被らない
         child: Container(
-          child: Text("feed page"),
+          child: FutureBuilder(
+            future: articles,
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              List<Widget> children = [];
+              if(snapshot.connectionState == ConnectionState.done){
+                if(snapshot.hasData){
+                  children = [
+                    Flexible(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index){
+                        return Container(
+                          child: Text(snapshot.data[index].title),
+                        );
+                      }),
+                    )
+                  ];
+
+                }else{
+                  children = <Widget>[
+                    const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Error: ${snapshot.error}'),
+                    )
+                  ];
+                }
+              }else{
+                children = const <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  )
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            }
+
+          ),
         ),
       ),
     );
--- lib/services/qiita_client.dart
+++ lib/services/qiita_client.dart
@@ -8,7 +8,6 @@ class QiitaClient {
     final url = 'https://qiita.com/api/v2/items';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      print(response.statusCode);
       final List<dynamic> jsonArray = json.decode(response.body);
       return jsonArray.map((json) => Article.fromJson(json)).toList();
     } else {
